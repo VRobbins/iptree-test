a8ee1ab62a24aad7a2ce3fbe4f3d16fe00c4272e
vincent.robbins@edgecast.com <vincent.robbins@edgecast.com>
2022-06-08 12:27:09 -0700

    remove all instances of rss

--- nms2/nms.h
+++ nms2/nms.h
@@ -25,10 +25,6 @@
 //: includes
 //: ----------------------------------------------------------------------------
 #include <iostream>
-#include <unistd.h>
-#include <sys/resource.h>
-#include <mach/mach.h>
-
 #include <map>
 #include <set>
 #include <list>
@@ -58,14 +54,6 @@ public:
         ~nms();
         int32_t add(const char *a_buf, uint32_t a_buf_len);
         int32_t contains(bool &ao_match, const char *a_buf, uint32_t a_buf_len);
-        size_t currentRSS()
-        {
-            struct mach_task_basic_info info;
-            mach_msg_type_number_t count = MACH_TASK_BASIC_INFO_COUNT;
-            if (task_info(mach_task_self(), MACH_TASK_BASIC_INFO, (task_info_t)&info, &count) == KERN_SUCCESS)
-                return (size_t)info.resident_size;
-            return (size_t)0; /* query failed */
-        }
 private:
         // -------------------------------------------------
         // private types
